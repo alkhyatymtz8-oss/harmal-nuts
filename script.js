02a62f43febb9e5b2c3103537353e5c8cc49aeb1
alkhyatymtz8-oss <alkhyatymtz8@gmail.com>
2026-05-04 04:36:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,56 +1,44 @@
 function goToProducts() {
-
-document.getElementById("products").scrollIntoView({
-    behavior:"smooth"
-});
+  document.getElementById("products").scrollIntoView({
+    behavior: "smooth"
+  });
 }
-const products =
-[
-    {
-        name:"زعقة" ,
-        price:"800" ,
-        image:"zaaqa.jpg"
-    },
 
-
-{
-        name:"قرع" ,
-        price:"2100" ,
-        image:"qara.jpg"
-    },
-
-
-    
-    {
-        name:"دوار الشمس" ,
-        price:"1300" ,
-        image:"doar.jpg"
-    },
-
-
-
-    {
-        name:"فول سوداني" ,
-        price:"900" ,
-        image:"hbazez.jpg"
-    }
-
+const products = [
+  {
+    name: "زعقة",
+    price: "800",
+    image: "zaaqa.jpg"
+  },
+  {
+    name: "قرع",
+    price: "2100",
+    image: "qara.jpg"
+  },
+  {
+    name: "دوار الشمس",
+    price: "1300",
+    image: "doar.jpg"
+  },
+  {
+    name: "فول سوداني",
+    price: "900",
+    image: "hbazez.jpg"
+  }
 ];
 
-const container =
-document.getElementById("products-container");
+const container = document.getElementById("products");
 
- 
-products.forEach((product) => {
-        
-    container.innerHTML+= `
+// عرض المنتجات
+products.forEach(product => {
+  container.innerHTML += `
     <div class="product">
-    <img src="${product.image}"width="150">
-    <h3>${product.name}</h3>
-    <p${product.price}>ريال</p>
-    <a href="https://wa.me/967773000738?text20% مرحبا اريد طلب منتج ${product.name}" target="blank_">اطلب الان
-</a>
+      <img src="${product.image}" width="150">
+      <h3>${product.name}</h3>
+      <p>${product.price} ريال</p>
+      <a href="https://wa.me/967773000738?text=مرحبا اريد طلب ${product.name}" target="_blank">
+        اطلب الآن
+      </a>
     </div>
-    `;
-    
+  `;
 });
