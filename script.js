ef3d2f7c67163cf24217d695dd843ce0e4043fe4
alkhyatymtz8-oss <alkhyatymtz8@gmail.com>
2026-05-04 03:31:46 +0300

    Add files via upload
    upload project

---
+++ script.js
@@ -0,0 +1,56 @@
+function goToProducts() {
+
+document.getElementById("products").scrollIntoView({
+    behavior:"smooth"
+});
+}
+const products =
+[
+    {
+        name:"زعقة" ,
+        price:"800" ,
+        image:"images/zaaqa.jpg"
+    },
+
+
+{
+        name:"قرع" ,
+        price:"2100" ,
+        image:"images/qara.jpg"
+    },
+
+
+    
+    {
+        name:"دوار الشمس" ,
+        price:"1300" ,
+        image:"images/doar.jpg"
+    },
+
+
+
+    {
+        name:"فول سوداني" ,
+        price:"900" ,
+        image:"images/hbazez.jpg"
+    }
+
+];
+
+const container =
+document.getElementById("products-container");
+
+ 
+products.forEach((product) => {
+        
+    container.innerHTML+= `
+    <div class="product">
+    <img src="${product.image}"width="150">
+    <h3>${product.name}</h3>
+    <p${product.price}>ريال</p>
+    <a href="https://wa.me/967773000738?text20% مرحبا اريد طلب منتج ${product.name}" target="blank_">اطلب الان
+</a>
+    </div>
+    `;
+    
+});
